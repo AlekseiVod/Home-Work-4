3a010a41c1a436665741fab0e1e9130476b8a747
AlekseiVod <vodianitskii.aleksei@mail.ru>
2024-06-21 09:10:36 +1000

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string = input('Введите Ваше Имя и Фамилию: ')
+len(my_string)
+print('Длина вашего имени и фамилии в количестве букв ',len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+
